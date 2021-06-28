72747988b8c76bb81826b0692e49a901fbb7744f
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-06-28 13:19:39 -0300

    fixing links

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const menuButton = document.querySelector('.header__menu-mobile')
+const links = document.querySelectorAll('.links')
 
 menuButton.addEventListener('click', () => {    
     menuButton.classList.toggle('header__menu-mobile--active')
     document.querySelector('.header__ul').classList.toggle('header__ul--active')
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menuButton.classList.toggle('header__menu-mobile--active')
+        document.querySelector('.header__ul').classList.toggle('header__ul--active')
+    })
 })
